a543c78e060fb285e0ff7517a2cba47213c1b665
Venkat Ram Taddi <tadivenkat@gmail.com>
2023-08-06 21:19:02 -0400

    Add calendar component to calendar module and export it to be used by other modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskListModule } from './task-list/task-list.module';
+import { CalendarModule } from './calendar/calendar.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, TaskListModule],
+  imports: [BrowserModule, AppRoutingModule, TaskListModule, CalendarModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/calendar/calendar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'calendar',
+  templateUrl: './calendar.component.html',
+  styleUrls: ['./calendar.component.css'],
+})
+export class CalendarComponent {}
