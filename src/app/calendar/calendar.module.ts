520dea405f08a35b3571a1a1e848c7e617e8f9ba
Venkat Ram Taddi <tadivenkat@gmail.com>
2023-08-07 12:10:50 -0400

    Added angular material calendar to Calendar component

--- src/app/calendar/calendar.module.ts
+++ src/app/calendar/calendar.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CalendarComponent } from './calendar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [CalendarComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+  ],
   exports: [CalendarComponent],
 })
 export class CalendarModule {}
