d4653e4da3dd00d7f6383b0583da2d491019d3a0
Venkat Ram Taddi <tadivenkat@gmail.com>
2023-08-06 21:34:41 -0400

    Adding angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskListModule } from './task-list/task-list.module';
 import { CalendarModule } from './calendar/calendar.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, TaskListModule, CalendarModule],
+  imports: [BrowserModule, AppRoutingModule, TaskListModule, CalendarModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
